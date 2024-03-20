0d39f3662f38271969d786cc102690305f4ec805
Sara-tfq <147254110+Sara-tfq@users.noreply.github.com>
2024-03-20 23:19:27 +0100

    Modification du scss pour l'affichage centrée des profils #6

--- front-end/src/app/quizzes/quiz-list/quiz-routing.module.ts
+++ front-end/src/app/quizzes/quiz-list/quiz-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
+//import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 
 const routes: Routes = [
 ];
