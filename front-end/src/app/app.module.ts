707fb9e5283bed6846fbf13f058984f3808a70a6
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.univ-cotedazur.fr>
2024-03-21 14:56:24 +0100

    feat: ajout du formulaire permettant la modification et la suppression des quiz #14

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,9 +9,9 @@ import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TitleComponent } from './head-title/title.component';
-import { QuizAddComponent } from './quizzes/quiz-add/quiz-add/quiz-add.component';
+import { QuizAddComponent } from './quizzes/quiz-add/quiz-add.component';
 import { QuizEditComponent } from './quizzes/quiz-edit/quiz-edit.component';
-import { QuizDeleteComponent } from './quizzes/quiz-delete/quiz-delete/quiz-delete.component';
+import { QuizDeleteComponent } from './quizzes/quiz-delete/quiz-delete.component';
 
 @NgModule({
   declarations: [
