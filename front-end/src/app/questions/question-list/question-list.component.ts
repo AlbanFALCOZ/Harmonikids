c03680bac58ce98339b1bd39cb05f0fa3d1df944
Alban FALCOZ <alban.falcoz@orange.fr>
2024-04-08 10:04:03 +0200

    Version Alban

--- front-end/src/app/questions/question-list/question-list.component.ts
+++ front-end/src/app/questions/question-list/question-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Question, Answer } from '../../../models/question.model';
+import { Question } from '../../../models/question.model';
 import { QuestionService } from '../../../services/question.service';
-import { QuestionType } from '../../../models/question.model';
 import { TitleService } from '../../../services/title.service';
+import { ScoreService } from './score-service-component';
 
 
 @Component({
@@ -14,25 +14,23 @@ import { TitleService } from '../../../services/title.service';
 export class QuestionListComponent implements OnInit {
 
   public questionList: Question[] = [];
-  selectedAnswer: Answer[] = [];
+
   currentQuestionIndex: number = 0;
-  QuestionType = QuestionType;
-  
-  showSuccessMessage: boolean = false;
-  showFailureMessage: boolean = false;
 
-  hintText: string | undefined;
-  hintImageUrl: string | undefined;
-  private hintAudio: HTMLAudioElement | null = null;
+  public numberOfQuestionsAnswered: number = 0;
+  public numberOfCorrectAnswers: number = 0;
 
-  private messageTimeout: any;
 
-  private successAudio = new Audio('assets/img/good.mp3');
-
-  constructor(private router: Router, public questionService: QuestionService) {
+  constructor(private router: Router, public questionService: QuestionService, private scoreService: ScoreService) {
     this.questionService.questions$.subscribe((questions: Question[]) => {
       this.questionList = questions;
     });
+    this.scoreService.numberOfQuestionsAnswered$.subscribe(score1 => {
+      this.numberOfQuestionsAnswered = score1;
+    });
+    this.scoreService.numberOfCorrectAnswers$.subscribe(score2 => {
+      this.numberOfCorrectAnswers = score2;
+    })
 
   }
 
@@ -44,81 +42,17 @@ export class QuestionListComponent implements OnInit {
     if (this.currentQuestionIndex < this.questionList.length - 1) {
       this.currentQuestionIndex++;
     }
-    this.resetMessages();
   }
 
   previousQuestion() {
     if (this.currentQuestionIndex > 0) {
       this.currentQuestionIndex--;
     }
-    this.resetMessages();
-
   }
   
-  
-  validateQuestion(): void {
-    if (this.selectedAnswer.length === 0) {
-      alert('Veuillez sélectionner au moins une réponse avant de valider.');
-      return;
-    }
-  
-    const currentQuestion = this.questionList[this.currentQuestionIndex];
-    const correctAnswers = currentQuestion.answers.filter(a => a.isCorrect);
-  
-    this.resetMessages();
-
-    const selectedAreAllCorrect = this.selectedAnswer.every(sa => sa.isCorrect);
-    const allCorrectAnswersSelected = correctAnswers.length === this.selectedAnswer.length &&
-                                      correctAnswers.every(ca => this.selectedAnswer.some(sa => sa.value === ca.value));
-  
-    if (selectedAreAllCorrect && allCorrectAnswersSelected) {
-      this.showSuccessMessage = true;
-      this.successAudio.play();
-    } else {
-      this.showFailureMessage = true;
-      const hint = this.questionList[this.currentQuestionIndex].hint;
-      this.showHint(hint);
-    }
-  
-    this.messageTimeout = setTimeout(() => {
-      this.showSuccessMessage = false;
-      this.showFailureMessage = false;
-      this.hintText = undefined;
-      this.hintImageUrl = undefined;
-    }, 8000);
+  generateArray(num: number): any[] {
+    if (num > 12) num = 12;
+    return Array(num);
   }
 
-
-  showHint(hint: any) {
-    if (this.hintAudio) {
-      this.hintAudio.pause();
-    }
-
-    if (hint) {
-      if (hint.audioUrl) {
-        this.hintAudio = new Audio(hint.audioUrl);
-        this.hintAudio.play();
-      }
-      this.hintText = hint.text;
-      this.hintImageUrl = hint.imageUrl;
-    }
-  }
-  
-  
-  resetMessages() {
-    clearTimeout(this.messageTimeout);
-    this.showSuccessMessage = false;
-    this.showFailureMessage = false;
-    this.hintText = undefined;
-    this.hintImageUrl = undefined;
-    if (this.hintAudio) {
-      this.hintAudio.pause();
-      this.hintAudio.currentTime = 0;
-    }
-  }
-
-  onAnswerSelected(answer: Answer[]): void {
-    this.selectedAnswer = answer.filter(a => a.isSelected);  
-    this.resetMessages();
-  }
 }
