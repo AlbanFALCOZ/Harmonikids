0370fbb3b7f615cf92e6179bccc88e1f08051ad9
Alban FALCOZ <alban.falcoz@orange.fr>
2024-04-08 09:16:51 +0200

    Changes to make master work

--- front-end/src/app/questions/multi-choice/multi-choice.component.ts
+++ front-end/src/app/questions/multi-choice/multi-choice.component.ts
@@ -1,73 +1,28 @@
-
 import { Component, Input, OnInit,  Output, EventEmitter } from '@angular/core';
 import { Question, Answer } from 'src/models/question.model';
-import { ScoreService } from '../question-list/score-service-component';
 
 @Component({
   selector: 'app-multi-choice',
   templateUrl: './multi-choice.component.html',
-  styleUrls: ['./multi-choice.component.scss']
+  styleUrl: './multi-choice.component.scss'
 })
-export class MultiChoiceComponent {
 
-  constructor(private scoreService: ScoreService) {}
 
-  showMessage: boolean = false;
-  message: string = '';
-  anwsersChosen: Answer[] = new Array();
+export class MultiChoiceComponent implements OnInit {
+
   @Input()
   multi?: Question;
   selectedAnswer: Answer[] = [];
 
   @Output() answerSelected = new EventEmitter<Answer[]>();
 
+  constructor() {
 
-  checkAnswer(selectedAnswer: Answer) {
-    if (selectedAnswer.isCorrect) {
-      if (!this.anwsersChosen.includes(selectedAnswer)) {
-        this.showMessage = true;
-        this.message = 'Bravo, mais tu es super fort et tu viens de gagner 15 étoiles!!!';
-        this.anwsersChosen.push(selectedAnswer);
-        this.scoreService.answerCorrect();
-        this.multi?.answers.forEach((item, index) => {
-          if (item === selectedAnswer) {
-            if (this.multi && this.multi.answers) {
-              const answer = document.getElementById("answer" + index);
-              answer?.classList.add("right-answer");
-            }
-          }
-        });
-      }
-    } else {
-      if (!this.anwsersChosen.includes(selectedAnswer))
-        {
-          this.anwsersChosen.push(selectedAnswer);
-          this.scoreService.answerWrong();
-        }
-      
-      this.multi?.answers.forEach((item, index) => {
-        if (item === selectedAnswer) {
-          if (this.multi && this.multi.answers) {
-            if (this.multi.answers.length > 2) {
-              const answer = document.getElementById("answer" + index);
-              answer?.classList.add("wrong-anwser");
-            }
-            else if (this.multi.answers.length == 2) {
-              this.message = 'Tu es sûr? Tu peux toujours changer de avis.';
-              this.showMessage = true;
-            }
-          }
+  }
 
-        }
-      });
-    }
+  ngOnInit(): void {
+  }
 
-
-    setTimeout(() => {
-      this.showMessage = false;
-    }, 4000);
-    
-    
   onSelectAnswer(answer: Answer): void {
    if (this.multi && this.multi.answers) {
     const correctAnswersCount = this.multi.answers.filter(ans => ans.isCorrect).length;
