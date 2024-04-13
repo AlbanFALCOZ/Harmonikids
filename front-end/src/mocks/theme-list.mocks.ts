4d7c5fb756ff14dd5bd33a179d9ade6bc868cb28
Sara-tfq <147254110+Sara-tfq@users.noreply.github.com>
2024-04-13 21:34:19 +0200

    Finalisation du dashboard #33

--- front-end/src/mocks/theme-list.mocks.ts
+++ front-end/src/mocks/theme-list.mocks.ts
@@ -6,47 +6,47 @@ export const THEME_LIST: Theme[] = [
     {
         id: '1',
         name: 'La nourriture',
-        description: 'Description du quiz 1',
-        numberOfQuizzes: 1,
+        description: 'Description du theme 1',
+        numberOfQuizzes: 3,
         quizzes: QUIZ_LIST,
         image: 'assets/img/food.jpg',
     },
     {
         id: '2',
         name: 'Le sport',
-        description: 'Description du quiz 2',
-        numberOfQuizzes: 1,
+        description: 'Description du theme 2',
+        numberOfQuizzes: 8,
         quizzes: QUIZ_LIST,
         image: 'assets/img/sports.png'
     },
     {
         id: '3',
         name: 'Les pays du monde',
-        description: 'Description du quiz 3',
-        numberOfQuizzes: 1,
+        description: 'Description du theme 3',
+        numberOfQuizzes: 5,
         quizzes: QUIZ_LIST,
         image: 'assets/img/drapeaux.jpg'
     },
     {
         id: '4',
         name: 'L\'astronomie',
-        description: 'Description du quiz 3',
-        numberOfQuizzes: 1,
+        description: 'Description du theme 4',
+        numberOfQuizzes: 2,
         quizzes: QUIZ_LIST,
         image: 'assets/img/etoile.jpg'
     },
     {
         id: '5',
         name: 'Les mathèmatiques',
-        description: 'Description du quiz 3',
-        numberOfQuizzes: 1,
+        description: 'Description du theme 5',
+        numberOfQuizzes: 3,
         quizzes: QUIZ_LIST,
         image: 'assets/img/maths.jpg'
     },
     {
         id: '6',
         name: 'La langue française',
-        description: 'Description du quiz 3',
+        description: 'Description du theme 6',
         numberOfQuizzes: 1,
         quizzes: QUIZ_LIST,
         image: 'assets/img/maths.jpg'
