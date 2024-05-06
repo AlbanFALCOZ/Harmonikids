6de3b500d40a3a552ea668b84b5841fa015656fe
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.univ-cotedazur.fr>
2024-05-06 12:06:18 +0200

    ajout de la route des quiz

--- front-end/src/services/quiz.service.ts
+++ front-end/src/services/quiz.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { Quiz } from '../models/quiz.model';
 import { QUIZ_LIST } from '../mocks/quiz-list.mock';
 import { Question } from '../models/question.model';
+import { serverUrl, httpOptionsBase } from '../configs/server.config';
 
 @Injectable({
   providedIn: 'root'
@@ -35,49 +36,47 @@ export class QuizService {
 
   public quizSelected$: Subject<Quiz> = new Subject();
 
-  // private quizUrl = serverUrl + '/quizzes';
-  //private questionsPath = 'questions';
+  private quizUrl = serverUrl + '/quizzes';
+  private questionsPath = 'questions';
 
-  // private httpOptions = httpOptionsBase;
+  private httpOptions = httpOptionsBase;
 
   constructor(private http: HttpClient) {
     this.retrieveQuizzes();
   }
 
   retrieveQuizzes(): void {
-    // this.http.get<Quiz[]>(this.quizUrl).subscribe((quizList) => {
-    //   this.quizzes = quizList;
-    //   this.quizzes$.next(this.quizzes);
-    // });
-    this.quizzes$.next(this.quizzes);
+    this.http.get<Quiz[]>(this.quizUrl).subscribe((quizList) => {
+      this.quizzes = quizList;
+      this.quizzes$.next(this.quizzes);
+    });
   }
+
 
   addQuiz(quiz: Quiz): void {
-    console.log("Hello ! ");
-    this.quizzes.push(quiz);
-    this.quizzes$.next(this.quizzes);
+    this.http.post<Quiz>(this.quizUrl, quiz, this.httpOptions).subscribe(() => this.retrieveQuizzes());
   }
-  
 
   setSelectedQuiz(quizId: string): void {
-    const selectedQuiz = this.quizzes.find(quiz => quiz.id === quizId);
-    if (selectedQuiz) {
-      this.quizSelected$.next(selectedQuiz);
-    }
-    
+    const urlWithId = this.quizUrl + '/' + quizId;
+    this.http.get<Quiz>(urlWithId).subscribe((quiz) => {
+      this.quizSelected$.next(quiz);
+    });
   }
 
   deleteQuiz(quiz: Quiz): void {
-    this.quizzes = this.quizzes.filter( q => q != quiz);
-    this.quizzes$.next(this.quizzes);
+    const urlWithId = this.quizUrl + '/' + quiz.id;
+    this.http.delete<Quiz>(urlWithId, this.httpOptions).subscribe(() => this.retrieveQuizzes());
   }
 
   addQuestion(quiz: Quiz, question: Question): void {
-    
+    const questionUrl = this.quizUrl + '/' + quiz.id + '/' + this.questionsPath;
+    this.http.post<Question>(questionUrl, question, this.httpOptions).subscribe(() => this.setSelectedQuiz(quiz.id));
   }
 
   deleteQuestion(quiz: Quiz, question: Question): void {
-    
+    const questionUrl = this.quizUrl + '/' + quiz.id + '/' + this.questionsPath + '/' + question.id;
+    this.http.delete<Question>(questionUrl, this.httpOptions).subscribe(() => this.setSelectedQuiz(quiz.id));
   }
 
 
