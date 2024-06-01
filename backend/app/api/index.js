76a9dd9cfdc5efaf9259f8f4409decb8c7228f0b
Sara-tfq <sara.taoufiq@etu.univ-cotedazur.fr>
2024-06-02 00:44:33 +0200

    Feat:Ajout d'un formulaire pour pouvoir ajouter des questions depuis l'ajout du quiz directement #97

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -3,20 +3,19 @@ const userRouter = require('./user')
 
 
 const QuizzesRouter = require('./quizzes')
-//const UsersRouter = require('./users')
-//const QuestionsRouter = require('./quizzes/questions')
-//const AnswersRouter = require('./quizzes/questions/answers')
+const QuestionsRouter = require('./quizzes/questions')
+const AnswersRouter = require('./quizzes/questions/answers')
 
 
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
-<<<<<<< HEAD
+
 router.use('/user', userRouter)
-=======
+
 router.use('/quizzes', QuizzesRouter)
-//router.use('/questions',QuestionsRouter)
-//router.use('/answers', AnswersRouter)
->>>>>>> a249d8910ef9e0f82fb4f66cb8e49b037f60988b
+router.use('/questions',QuestionsRouter)
+router.use('/answers', AnswersRouter)
+
 
 module.exports = router
