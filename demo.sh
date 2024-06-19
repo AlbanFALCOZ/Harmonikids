0fb5c6e76f4873b1d43e356c0f05c8182e6b02ab
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.univ-cotedazur.fr>
2024-06-19 12:42:55 +0200

    feat: docker #127

--- demo.sh
+++ demo.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+cd backend
+docker build -t backend-image .
 
-# Build backend image
-docker build -t backend-image -f /backend/Dockerfile .
+cd ..
 
-# Build frontend image
-docker build -t frontend-image -f /front-end/Dockerfile .
+cd front-end
+docker build -t frontend-image .
 
-# Run backend container
 docker run -d --name backend-container -p 8081:9428 backend-image
 
-# Run frontend container
 docker run -d --name frontend-container -p 8080:80 frontend-image
